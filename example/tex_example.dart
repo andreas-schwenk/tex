28d9feb42cfe517bf0fea427af77490570f28e7f
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2023-03-03 15:18:57 +0100

    Update tex_example.dart

--- example/tex_example.dart
+++ example/tex_example.dart
@@ -1,6 +1,11 @@
 import 'package:tex/tex.dart';
+//import 'dart:tex';
 
 void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
+  var tex = TeX();
+  var svgImageData = tex.tex2svg("x^2 + y^2");
+  if (svgImageData.isEmpty) {
+    print('Errors occurred: ${tex.error}');
+  }
+  print(svgImageData);
 }
