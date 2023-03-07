e384022e9f46206eaaaf216e5f5097618bd300c8
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2023-03-07 16:10:04 +0100

    Update tex_test.dart

--- test/tex_test.dart
+++ test/tex_test.dart
@@ -8,7 +8,8 @@ import 'package:tex/tex.dart';
 
 void main() {
   var tex = TeX();
-  var src = "\\frac x {y+1}"; //"\\frac x{ \\sum_1^{{6}} w } \\cdot 5";
+  var src =
+      "\\sqrt 3"; //"\\frac x {y+1}"; //"\\frac x{ \\sum_1^{{6}} w } \\cdot 5";
   //"x^2  + {4*5}";
   var output = '';
   var paintBox = true;
