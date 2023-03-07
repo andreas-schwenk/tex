52a60ff17a1469bd5245b62e0448f12e99d2e055
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2023-03-07 16:09:47 +0100

    Update tex_base.dart

--- lib/src/tex_base.dart
+++ lib/src/tex_base.dart
@@ -2,8 +2,7 @@
 /// (c) 2023 by Andreas Schwenk <mailto:contact@compiler-construction.com>
 /// License: GPL-3.0-or-later
 
-import 'package:tex/src/node.dart';
-
+import 'config.dart';
 import 'gen.dart';
 import 'lex.dart';
 import 'parse.dart';
@@ -16,7 +15,10 @@ class TeX {
   final Lex _lex = Lex();
 
   /// Recently parsed TeX input, stringified.
-  String _lastParsed = '';
+  String _parsed = '';
+
+  /// Width of recently parsed TeX rendering.
+  int _width = 0;
 
   /// Error messages.
   String _error = '';
@@ -25,13 +27,18 @@ class TeX {
   double _scalingFactor = 1.0;
 
   /// Gets the parsed and stringified TeX input for the last call of [tex2svg].
-  String get lastParsed {
-    return _lastParsed;
+  String get parsed {
+    return _parsed;
   }
 
   /// Gets the error for the last call of [tex2svg].
   String get error {
     return _error;
+  }
+
+  /// Gets the width of the last rendering.
+  int get width {
+    return _width;
   }
 
   /// Sets the scaling factor
@@ -43,87 +50,65 @@ class TeX {
   ///
   /// A debug output can be achieved by rendering bounding boxes via [paintBox].
   String tex2svg(String src, [paintBox = false]) {
+    // tokenize input
     _lex.set(src);
     try {
+      // recursively parse input into TexNodes
       var root = parseTexList(_lex, false);
-      _lastParsed = root.toString();
+      // stringify TexNodes for debug purposes
+      _parsed = root.toString();
+      // typeset, i.e. calculate relative positions and scaling
       typeset(root, 0, 0);
-      var commands = gen(paintBox, root, 4);
-      var xx = _calcMinY(root);
-      print('xx = $xx');
-      final int belowHeight = -xx + 275; //275; // TODO
-      int minX = 0;
-      int minY = -root.height;
-      int width = root.width;
-      int height = root.height + belowHeight - xx;
-      var defs = '';
+      // transform local coordinates into global coordinates
+      root.calculateGlobalCoordinates();
+      // vertically move all glyphs, s.t. y=0 is exactly where the two lines of
+      // glyph "x" intersect.
+      root.translate(0, globalTranslateY.toDouble());
+      // generate SVG data
+      var svgDATA = gen(paintBox, root, 4);
+      // calculate the view box of the SVG image; note that the y-axis is
+      // mirrored, since glyphs data in "svg.dart" are mirrored.
+      var globalMin = root.getGlobalMinY();
+      var globalMax = root.getGlobalMaxY();
+      var deltaY = -globalMin > globalMax ? -globalMin : globalMax;
+      int viewBoxX = 0;
+      int viewBoxWidth = root.width;
+      int viewBoxY = -deltaY.round();
+      int viewBoxHeight = (deltaY * 2.0).ceil();
+      // make sure, that the view box width is always positive;
+      // otherwise some SVG libraries might crash..
+      if (viewBoxWidth == 0) viewBoxWidth = 100;
+      // get the image width in pixels
+      _width = (viewBoxWidth * 0.02 * _scalingFactor).round();
+      // generate SVG paths, i.e. polygons for all actually used glyphs
+      var svgPaths = '';
       Set<String> usedLetters = {};
-      _getUsedGlyphs(usedLetters, root);
+      root.getActuallyUsedGlyphs(usedLetters);
       for (var id in usedLetters) {
         var d = svgData[id];
-        defs += '    <path id="$id" d="$d"></path>\n';
+        svgPaths += '    <path id="$id" d="$d"></path>\n';
       }
-      if (width == 0) width = 100;
+      // optionally generate bounding rectangles around glyphs for debugging
+      // purposes
       String boundingBoxes = '';
       if (paintBox) {
-        boundingBoxes = '    '
-            '<rect x="0" y="${-belowHeight}" width="$width" height="$height"'
-            ' fill="none" stroke="rgb(200,200,200)" stroke-width="50"></rect>\n'
-            '    '
-            '<rect x="0" y="0" width="$width" height="${height - belowHeight}"'
-            ' fill="none" stroke="rgb(200,200,200)"'
-            ' stroke-width="25"></rect>\n';
+        boundingBoxes = genBoundingBoxes(root);
       }
-      commands =
-          '  <g stroke="currentColor" fill="currentColor" stroke-width="0"'
-          ' transform="scale(1,-1)">\n$boundingBoxes$commands  </g>';
-      var svg =
-          '<svg width="${(width * 0.02 * _scalingFactor).round()}" style="" xmlns="http://www.w3.org/2000/svg" role="img"'
-          ' focusable="false" viewBox="$minX $minY $width $height"'
+      // create final output
+      var output =
+          '<svg width="$_width" style="" xmlns="http://www.w3.org/2000/svg" role="img"'
+          ' focusable="false" viewBox="$viewBoxX $viewBoxY $viewBoxWidth $viewBoxHeight"'
           ' xmlns:xlink="http://www.w3.org/1999/xlink">\n'
-          '  <defs>\n$defs  </defs>\n$commands\n'
+          '  <defs>\n$svgPaths  </defs>\n'
+          '$boundingBoxes'
+          '  <g transform="scale(1,-1)">\n'
+          '$svgDATA'
+          '  </g>'
           '</svg>\n';
-      return svg;
+      return output;
     } catch (e) {
       _error = e.toString();
       return "";
-    }
-  }
-
-  int _calcMinY(TeXNode node) {
-    int minY = 0;
-    for (var item in node.items) {
-      int itemY = node.y + (_calcMinY(item) * item.scaling).round();
-      if (itemY < minY) minY = itemY;
-    }
-    for (var arg in node.args) {
-      int argY = node.y + (_calcMinY(arg) * arg.scaling).round();
-      if (argY < minY) minY = argY;
-    }
-    if (node.sub != null) {
-      var sub = node.sub as TeXNode;
-      int subY = node.y + (_calcMinY(sub) * sub.scaling).round();
-      if (subY < minY) minY = subY;
-    }
-    return minY;
-  }
-
-  /// Gets a set of all actually used glyphs for a [node].
-  void _getUsedGlyphs(Set<String> usedLetters, TeXNode node) {
-    if (node.svgPathId.isNotEmpty) {
-      usedLetters.add(node.svgPathId);
-    }
-    for (var item in node.items) {
-      _getUsedGlyphs(usedLetters, item);
-    }
-    for (var arg in node.args) {
-      _getUsedGlyphs(usedLetters, arg);
-    }
-    if (node.sub != null) {
-      _getUsedGlyphs(usedLetters, node.sub as TeXNode);
-    }
-    if (node.sup != null) {
-      _getUsedGlyphs(usedLetters, node.sup as TeXNode);
     }
   }
 }
