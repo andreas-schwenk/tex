8a04fdd5c2759992f43828075894e551324a69e3
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2023-03-07 16:09:22 +0100

    Create config.dart

---
+++ lib/src/config.dart
@@ -0,0 +1,8 @@
+/// tex - a tiny TeX engine
+/// (c) 2023 by Andreas Schwenk <mailto:contact@compiler-construction.com>
+/// License: GPL-3.0-or-later
+
+// TODO: move ALL constants to here
+
+const int standardFontHeight = 750;
+const int globalTranslateY = -210;
