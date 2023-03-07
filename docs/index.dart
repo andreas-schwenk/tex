b3f37fb228360b4b7a6342aa2ec156aaa36bda47
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2023-03-07 16:09:14 +0100

    Update index.dart

--- docs/index.dart
+++ docs/index.dart
@@ -8,7 +8,13 @@ import 'dart:html';
 import 'package:tex/tex.dart';
 import 'package:tex/src/tab.dart' as tab;
 
-final examples = ['y(x)=x^2', '\\frac 1 {x+1}', '\\sin x', '\\sin(x)'];
+final examples = [
+  'y(x)=x^2',
+  '\\frac 1 {x+1}',
+  '\\sin x',
+  '\\sin(x)',
+  "\\sqrt{x+1}"
+];
 
 void main() {
   print("tex - a tiny TeX engine");
@@ -26,20 +32,28 @@ void main() {
   // inline equation examples
   var inlineExamples = ["x^2+y^2", "f(x)=\\frac 1 2 x"];
   for (var k = 0; k < inlineExamples.length; k++) {
-    var tex = TeX();
-    var imgData = tex.tex2svg(inlineExamples[k]);
-    var outputBase64 = base64Encode(utf8.encode(imgData));
-    var img = document.createElement('img') as ImageElement;
-    img.src = "data:image/svg+xml;base64,$outputBase64";
-    img.style.verticalAlign = "bottom";
-    var span = querySelector('#example-inline-${k + 1}') as SpanElement;
-    span.innerHtml = '';
-    span.style.backgroundColor = '#EEEEEE';
-    span.style.paddingLeft = "3px";
-    span.style.paddingRight = "3px";
-    span.style.display = "inline-block";
-    span.style.verticalAlign = "top";
-    span.append(img);
+    for (var j = 0; j < 2; j++) {
+      var tex = TeX();
+      tex.scalingFactor = j == 0 ? 1.0 : 1.25;
+      var imgData = tex.tex2svg(inlineExamples[k]);
+      var outputBase64 = base64Encode(utf8.encode(imgData));
+      var img = document.createElement('img') as ImageElement;
+      img.src = "data:image/svg+xml;base64,$outputBase64";
+      img.style.verticalAlign = "bottom";
+      var spanId = j == 0
+          ? '#example-inline-${k + 1}'
+          : '#example-inline-large-${k + 1}';
+      var span = querySelector(spanId) as SpanElement;
+      span.innerHtml = '';
+      //span.style.borderColor = '#EEEEEE';
+      //span.style.borderStyle = 'solid';
+      //span.style.borderWidth = '2px';
+      span.style.paddingLeft = "3px";
+      span.style.paddingRight = "3px";
+      span.style.display = "inline-block";
+      span.style.verticalAlign = "middle";
+      span.append(img);
+    }
   }
 
   // examples
@@ -93,6 +107,9 @@ void main() {
   glyphTable.innerHtml = '';
   for (var texSrc in tab.table.keys) {
     var rowData = tab.table[texSrc] as Map<String, Object>;
+
+    if (texSrc == '\\sqrt') texSrc = '\\sqrt{}';
+
     var output = tex.tex2svg(texSrc, true);
     if (output.isEmpty) {
       print(tex.error);
@@ -131,6 +148,7 @@ void main() {
 void typeset() {
   var src = (querySelector('#tex-input') as InputElement).value as String;
   document.getElementById('tex-rendering')?.innerHtml = '';
+  document.getElementById('tex-rendering-with-boxes')?.innerHtml = '';
   var tex = TeX();
   print(src);
   tex.scalingFactor = 2.0;
@@ -141,8 +159,12 @@ void typeset() {
       var outputBase64 = base64Encode(utf8.encode(output));
       var img = document.createElement('img') as ImageElement;
       img.src = "data:image/svg+xml;base64,$outputBase64";
-      document.getElementById('tex-term')?.innerHtml = tex.lastParsed;
-      document.getElementById('tex-rendering')?.append(img);
+      document.getElementById('tex-term')?.innerHtml = tex.parsed;
+      if (i == 0) {
+        document.getElementById('tex-rendering')?.append(img);
+      } else {
+        document.getElementById('tex-rendering-with-boxes')?.append(img);
+      }
     } else {
       document.getElementById('tex-term')?.innerHtml = tex.error;
     }
