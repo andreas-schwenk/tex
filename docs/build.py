8439196a377b63f496c4c91c2c75df8ee9cb6d0b
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2023-03-17 08:34:17 +0100

    website updates

--- docs/build.py
+++ docs/build.py
@@ -2,17 +2,18 @@
 # (c) 2023 by Andreas Schwenk <mailto:contact@compiler-construction.com>
 # License: GPL-3.0-or-later
 
-# This script updates the Script version numbers in file "index.html"
-# to force browsers to load the most recent version.
-
 import time
 
-# get unit time
+# --------
+# Update the version number attribute(s) in given files,
+# to force browsers to load the most recent version.
+# --------
+
+# get unix time
 unix_time = int(time.time())
 
-files = ["index.html","compare.html"]
-
-for file in files:
+# for all files
+for file in ["index.html", "compare.html"]:
 
     # read file
     f = open(file, "r")
@@ -29,3 +30,29 @@ for file in files:
     f = open(file, "w")
     f.write("".join(lines))
     f.close()
+
+# --------
+# Convert the changelog for the website.
+# --------
+
+# read file
+f = open("../CHANGELOG.md", "r")
+lines = f.readlines()
+f.close()
+
+# create HTML
+output = ''
+for line in lines:
+    line = line[:-1]
+    if line.startswith('##'):
+        if len(output) > 0:
+            output += '</ul>\n'
+        output += '<h3>' + line[3:] + '</h3>\n<ul>\n'
+    elif line.startswith('-'):
+        output += '  <li>' + line[1:].strip() + '</li>\n'
+output += '</ul>\n'
+
+# write file
+f = open("changelog.txt", "w")
+f.write(output)
+f.close()
